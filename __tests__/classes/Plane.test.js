2d4d94b2eee0d8dca347f574ee5c6b12086cb055
ktinson <monikai.tinson@verizon.com>
2024-09-12 16:56:03 -0500

    Just ulled and removed an extra test

--- __tests__/classes/Plane.test.js
+++ __tests__/classes/Plane.test.js
@@ -21,14 +21,14 @@ describe("Plane Class", () => {
         expect(pl1.getPassengers()).toEqual([]);
     });
 
-    //Add a passenger and verify the passenger is added
-    test('should add a passenger to the passengers array', () => {
-        const pl1 = new Plane('Delta',"company", 'LAX', passengers=[]);
-        const p1 = new Person('John Doe', 'LAX');
-        pl1.addPassenger(p1);
-        expect(pl1.passengers.length).toBe(1);
-        expect(pl1.passengers[0]).toBe(p1);
-    });
+    // //Add a passenger and verify the passenger is added
+    // test('should add a passenger to the passengers array', () => {
+    //     const pl1 = new Plane('Delta',"company", 'LAX', passengers=[]);
+    //     const p1 = new Person('John Doe', 'LAX');
+    //     pl1.addPassenger(p1);
+    //     expect(pl1.getPassengers().length).toBe(1);
+    //     expect(pl1.passengers[0]).toBe(p1);
+    // });
 });
   //Verify that passengers initialize as an empty array
   test("should initialize passengers as an empty array", () => {
@@ -39,9 +39,9 @@ describe("Plane Class", () => {
   //Add a passenger and verify the passenger is added
   test("should add a passenger to the passengers array", () => {
     const pl1 = new Plane("Delta", "LAX", "NYC", passenger = []);
-    const p1 = new Person("John Doe");
-    pl1.addPassenger(p1);
-    expect(pl1.getPassengers()).toEqual(passenger.length);
+    // const p1 = new Person("John Doe");
+    pl1.addPassenger(1);
+    expect(pl1.getPassengers().length).toBe(1)
   });
-});
+
 
